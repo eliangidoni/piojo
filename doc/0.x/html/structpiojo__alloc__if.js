6150a408f88831aff773c7f28d540d55b97e03c7
Elian Gidoni <elian@braceloop.com>
2014-03-22 18:09:38 -0300

    Docs update.

--- doc/0.x/html/structpiojo__alloc__if.js
+++ doc/0.x/html/structpiojo__alloc__if.js
@@ -4,5 +4,6 @@ var structpiojo__alloc__if =
     [ "copy_cb", "structpiojo__alloc__if.html#a55e111b708d0352d397813ddd37f0bb9", null ],
     [ "finish_cb", "structpiojo__alloc__if.html#a718d26f96721f3a28cd42d64477979bd", null ],
     [ "free_cb", "structpiojo__alloc__if.html#affdbe06897bf9e82024a014f80ff3c6a", null ],
-    [ "init_cb", "structpiojo__alloc__if.html#a15b17f43c9b916f51218535b03792a28", null ]
+    [ "init_cb", "structpiojo__alloc__if.html#a15b17f43c9b916f51218535b03792a28", null ],
+    [ "realloc_cb", "structpiojo__alloc__if.html#a857080cabafe489e50254e66406a5277", null ]
 ];
