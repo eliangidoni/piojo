fd8f826e50c07138fe9154cb3de22f5e75fe7a7b
Elian Gidoni <elian@braceloop.com>
2015-04-05 04:48:38 +0000

    update doc.

--- doc/0.x/html/search/all_72.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['realloc_5fcb',['realloc_cb',['../structpiojo__alloc__if.html#a857080cabafe489e50254e66406a5277',1,'piojo_alloc_if::realloc_cb()'],['../structpiojo__alloc__kv__if.html#a4d4081311dfa5ea47e3c2d134e30f5c7',1,'piojo_alloc_kv_if::realloc_cb()']]]
-];
